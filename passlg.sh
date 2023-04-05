55ea636ad6b37e48c6ce9794d7df1e3f9bbd426e
Keerthi.P <117421912+Keerthi2000kithu@users.noreply.github.com>
2023-04-05 10:07:37 +0530

    Add files via upload

---
+++ passlg.sh
@@ -0,0 +1,14 @@
+read -p "Enter The Login Name:" username
+read -sp "Enter The Password:" password
+
+
+if [[ $username == "user"  && $password == "password" ]]
+then
+  echo -e "\nYou're Logged In\n"
+elif [ $username != "user" ] 
+then
+  echo -e "\nInvalid User Name\n"
+else
+  echo -e "\nInvalid Password\n"
+fi
+
